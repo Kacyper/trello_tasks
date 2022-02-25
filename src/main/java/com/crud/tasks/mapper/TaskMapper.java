fa2cba75ce5145b99f54bc1e7dd0b0228acc6724
Kacper <kksykiewicz@gmail.com>
2022-02-25 17:22:19 +0100

    29.1 Facade test - Task Mapper

--- src/main/java/com/crud/tasks/mapper/TaskMapper.java
+++ src/main/java/com/crud/tasks/mapper/TaskMapper.java
@@ -1,13 +1,15 @@
 package com.crud.tasks.mapper;
 
-import com.crud.tasks.domain.Task;
-import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.domain.*;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
+@Component
 public class TaskMapper {
 
     public Task mapToTask(final TaskDto taskDto) {
@@ -29,7 +31,6 @@ public class TaskMapper {
     public List<TaskDto> mapToTaskDtoList(final List<Task> taskList) {
         return taskList.stream()
                 .map(this::mapToTaskDto)
-                .collect(Collectors.toList());
+                .collect(toList());
     }
-
 }
